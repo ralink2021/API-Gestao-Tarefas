f93729b9fa4961fa1daa96aaaf674968b2c3905c
Rafael Oliveira <seguranca_pentest@icloud.com>
2021-06-28 17:34:35 -0300

    Inicio do Projeto

---
+++ src/main/java/com/esig/gestaotarefa/GestaotarefaApplication.java
@@ -0,0 +1,13 @@
+package com.esig.gestaotarefa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaotarefaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaotarefaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esig/gestaotarefa/GestaotarefaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esig.gestaotarefa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaotarefaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
