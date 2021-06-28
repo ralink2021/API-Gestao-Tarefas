25877e4c8949d17b215da36183439cf6708ecf79
Rafael Oliveira <seguranca_pentest@icloud.com>
2021-06-28 19:39:49 -0300

    Metodo GET - Tarefas

--- src/main/java/com/esig/gestaotarefa/controller/TarefaController.java
+++ src/main/java/com/esig/gestaotarefa/controller/TarefaController.java
@@ -1,7 +1,10 @@
 package com.esig.gestaotarefa.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +24,13 @@ public class TarefaController {
 	@PostMapping
 	public Tarefa createTarefa(@RequestBody Tarefa tarefa) {
 		return serviceTarefa.saveTarefa(tarefa);
+		
+	}
+	
+	/** Metodo GET - Para Listagem das TarefaS */
+	@GetMapping
+	public List<Tarefa> getTarefaList(){
+		return serviceTarefa.findAll();
 		
 	}
 
--- src/main/java/com/esig/gestaotarefa/service/TarefaService.java
+++ src/main/java/com/esig/gestaotarefa/service/TarefaService.java
@@ -1,5 +1,7 @@
 package com.esig.gestaotarefa.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +14,14 @@ public class TarefaService {
 	@Autowired
 	TarefaRepository repository;
 	
+	/* Serviço para Salvar uma Tarefas */
 	public Tarefa saveTarefa(Tarefa tarefa) {
 		return repository.save(tarefa);
+	}
+	
+	/* Serviço para Listar todas as Tarefas */
+	public List<Tarefa> findAll(){
+		return repository.findAll();
 	}
 	
 }
